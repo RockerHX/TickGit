df5214e712ab890e9d9cf2cc335818d143fb7113
RockerHX <rockerhx@gmail.com>
2026-06-07 21:36:28 +0800

    feat(commit): 接入 Commit Message 底部区域

--- src/lib/components/CommitDetailPanel.svelte
+++ src/lib/components/CommitDetailPanel.svelte
@@ -2,6 +2,7 @@
   import { onDestroy } from "svelte";
   import { locale, translate } from "$lib/i18n";
   import { createEventDispatcher } from "svelte";
+  import CommitMessagePanel from "$lib/components/CommitMessagePanel.svelte";
   import DiffViewer from "$lib/components/DiffViewer.svelte";
   import FileTypeIcon from "$lib/components/FileTypeIcon.svelte";
   import ResizeHandle from "$lib/components/ResizeHandle.svelte";
@@ -207,14 +208,6 @@
                 {tag}
               </span>
             {/each}
-          </div>
-        {/if}
-
-        {#if commitMeta?.body}
-          <div
-            class="mt-2 whitespace-pre-wrap text-[0.95rem] leading-6 text-slate-200"
-          >
-            {commitMeta.body}
           </div>
         {/if}
 
@@ -446,17 +439,20 @@
       on:mousedown={(event) => startFilesPaneResize(event.detail)}
     />
 
-    <DiffViewer
-      title={translate($locale, "diff.title")}
-      {selectedFilePath}
-      {selectedFile}
-      {diffResult}
-      {loadingDiff}
-      mode={diffViewMode}
-      {hideWhitespaceInDiff}
-      on:modeChange={(event) => dispatch("diffModeChange", event.detail)}
-      on:hideWhitespaceChange={(event) =>
-        dispatch("hideWhitespaceChange", event.detail)}
-    />
+    <div class="flex min-h-0 min-w-0 flex-col">
+      <DiffViewer
+        title={translate($locale, "diff.title")}
+        {selectedFilePath}
+        {selectedFile}
+        {diffResult}
+        {loadingDiff}
+        mode={diffViewMode}
+        {hideWhitespaceInDiff}
+        on:modeChange={(event) => dispatch("diffModeChange", event.detail)}
+        on:hideWhitespaceChange={(event) =>
+          dispatch("hideWhitespaceChange", event.detail)}
+      />
+      <CommitMessagePanel {commit} {commitMeta} />
+    </div>
   </div>
 </div>
